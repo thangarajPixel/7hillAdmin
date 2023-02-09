69c121cf7bd6eb00adf1ffd6bdd4cc5a8fef08b0
santhoshpixel <santhoshd.pixel@gmail.com>
2023-02-09 12:17:07 +0530

    contact us

--- database/migrations/2023_01_18_071812_create_contacts_table.php
+++ database/migrations/2023_01_18_071812_create_contacts_table.php
@@ -14,7 +14,7 @@ class CreateContactsTable extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email');
             $table->string('mobile');
