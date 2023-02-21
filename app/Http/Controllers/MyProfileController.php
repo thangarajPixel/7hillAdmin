3d64314dea8fc60efa7ffe8f9076d81913b6b33a
santhoshpixel <santhoshd.pixel@gmail.com>
2023-02-21 18:51:04 +0530

    p2

--- app/Http/Controllers/MyProfileController.php
+++ app/Http/Controllers/MyProfileController.php
@@ -60,7 +60,6 @@ class MyProfileController extends Controller
                     
                     $existID = User::find($id);
                     $deleted_file = $existID->image;
-                    dd("1111");
                     if($id)
                     {
                         $existID = User::find($id);
@@ -69,6 +68,7 @@ class MyProfileController extends Controller
                             File::delete($deleted_file);
                         }
                     }
+                    dd("22");
                    
                     if (!file_exists($folder_name)) {
                         mkdir($folder_name, 777, true);
